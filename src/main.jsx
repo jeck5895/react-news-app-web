a7117b4f0cf097f1abd6ad74d33b43312efe576d
Jerick L. <jerick.labasan@gcash.com>
2025-07-09 17:03:57 +0800

    implement react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import "@fortawesome/fontawesome-free/css/all.css";
 import './index.css'
-import App from './App.jsx'
+import Home from './views/Home.jsx'
+import WithNavbar from './layouts/WithNavbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<WithNavbar/>}>
+          <Route path='/' element={<Home/>}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
